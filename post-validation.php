8941f22f04fed2feea8d594bc04c8f03607a3c48
Samuel Smith <81743491+S3910951@users.noreply.github.com>
2024-02-15 23:14:15 +1000

    post validation

--- post-validation.php
+++ post-validation.php
@@ -1,14 +1,38 @@
 <?php
 include('tools.php');
 function validateBooking() {
-  $errors = [];
-  if ($_POST['customer']['name'] == '') {
-    $errors['customer']['name'] == "Name can't be blank";
-  } else {
+  global $moviesObject; 
+  $errors = []; 
+
+  // Validate movie code
+  if (empty($_POST['movie']) || !isset($moviesObject[$_POST['movie']])) {
+      header('Location: index.php'); 
+      exit();
   }
-  if ($_POST['customer']['email'] == '') {
-    $errors['customer']['email'] == "Email can't be blank";
-  } else {
+
+  // Validate day
+  if (empty($_POST['day']) || !array_key_exists($_POST['day'], $moviesObject[$_POST['movie']]['screenings'])) {
+      header('Location: index.php'); 
+      exit();
   }
+
+  // Validate seat selections
+  $seatSelected = false;
+  foreach ($_POST['seats'] as $type => $quantity) {
+      if (!is_numeric($quantity) || $quantity < 0 || $quantity > 10) {
+          header('Location: index.php'); 
+          exit();
+      }
+      if ($quantity > 0) $seatSelected = true;
+  }
+  if (!$seatSelected) $errors['seats'] = 'At least one seat must be selected.';
+
+  // Validate customer fields
+  if (!filter_var($_POST['customer']['email'], FILTER_VALIDATE_EMAIL)) {
+      $errors['customer']['email'] = 'Invalid email address.';
+  }
+
+  return $errors;
 }
+
 ?>
